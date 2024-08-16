e906fefd5308a19010bda219aafcf2706f9a6841
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-08-17 05:17:36 +0530

    react added

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Layout from "./Components/Layout/layout";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1>Ecommerce App</h1>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
