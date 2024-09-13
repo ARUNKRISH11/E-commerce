38c4ca1b1f1ee9cbc1b4be48cc60f446c9f6d1f2
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-09-14 04:53:56 +0530

    page not found

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
     <Layout>
-      <div>PageNotFound</div>
+      <div className="page-not-found">
+        <p>Page Not Found - 404!</p>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <Link to={"/"} className="btn ">
+          Back to Home
+        </Link>
+      </div>
     </Layout>
   );
 }
