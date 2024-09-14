b9e7ab52a51d6e7ea6d4aea430fdafb567af42af
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-09-14 05:55:47 +0530

    about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,7 +4,52 @@ import Layout from "../components/layout/Layout";
 function About() {
   return (
     <Layout>
-      <div>About</div>
+      <div>
+        <h1 className="page-title">About Us</h1>
+        <p className="page-sub-title">Welcome to Store Buddy</p>
+        <p className="page-content">
+          Store Buddy is an e-commerce webapp where you can find all the
+          necessary shopping items. We have achieved many milestones since our
+          inception, including completing over 1000 orders, having a customer
+          rating of 4.5 and above, and having a user base of over 1000
+          customers. We are committed to providing the best experience to our
+          customers and are constantly working to improve our services.
+        </p>
+        <p className="page-content">
+          Our customers have given us a rating of 4.5 and above and have
+          provided positive feedback about our services. We are grateful for
+          their support and are committed to continuing to provide the best
+          experience to them.
+        </p>
+        <p className="page-content">
+          If you want to download our webapp from the play store or apple store,
+          you can click on the following links:
+        </p>
+        <div className="d-flex justify-content-center">
+          <a
+            href="https://play.google.com/store/apps/details?id=com.storebuddy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/1200px-Google_Play_Store_badge_EN.svg.png"
+              alt="Get it on Google Play"
+              width="200"
+            />
+          </a>
+          <a
+            href="https://apps.apple.com/us/app/store-buddy/id1445391413"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg"
+              alt="Get it on App Store"
+              width="200"
+            />
+          </a>
+        </div>
+      </div>
     </Layout>
   );
 }
